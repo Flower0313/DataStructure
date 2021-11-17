bd3e5f3bbbc523844028daaee9efb08b58764bfa
holdenxiao <42406286+Flower0313@users.noreply.github.com>
2021-11-17 19:28:25 +0800

    #1 在github上修改

--- src/git/gitTest.java
+++ src/git/gitTest.java
@@ -19,6 +19,8 @@ public class gitTest {
 
         System.out.println("hhhhh");
         System.out.println("fix in dev in order to conflict");
+        System.out.println("fixed in github")
+
     }
 }
 
